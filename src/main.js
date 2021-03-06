ff1316ad74fb8d87249602517df155c4048d94cf
XiNGRZ <hi@xingrz.me>
2021-03-07 00:44:10 +0800

    Set up antd

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+import App from './App.vue';
 
 Vue.config.productionTip = false
 
+Vue.use(Antd);
+
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
